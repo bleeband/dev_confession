6197e8b2925e03f686ceb721ac6fe01378c08b4b
Marc-Andre Dufour <bleeband.dev@gmail.com>
2026-09-03 11:33:04 -0400

    add: leaderboard+buycredit

---
+++ src/app/buy-credits/page.tsx
@@ -0,0 +1,11 @@
+// page de classement de confessions par utilisateurs
+export default async function BuyCreditsPage(){
+    return(
+        <div className="min-h-screen bg-gray-900 via-purple-900/20 to-gray-900">
+                <p>
+                    Solde Actuel:
+                    Karma:
+                </p>
+        </div>
+    );
+}
---
+++ src/app/leaderboard/page.tsx
@@ -0,0 +1,10 @@
+// page de classement de confessions par utilisateurs
+export default async function LeaderboardPage(){
+    return(
+        <div className="min-h-screen bg-gray-900 via-purple-900/20 to-gray-900">
+                <p>
+                    Top Users:
+                </p>
+        </div>
+    );
+}
